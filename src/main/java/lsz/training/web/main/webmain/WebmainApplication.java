a109efbe703d49a113947da4544db7401d0a0f75
Luis <luissanzaguilar@gmail.com>
2025-08-11 13:47:32 +0200

    Initial commit

---
+++ src/main/java/lsz/training/web/main/webmain/WebmainApplication.java
@@ -0,0 +1,13 @@
+package lsz.training.web.main.webmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lsz/training/web/main/webmain/WebmainApplicationTests.java
@@ -0,0 +1,13 @@
+package lsz.training.web.main.webmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
